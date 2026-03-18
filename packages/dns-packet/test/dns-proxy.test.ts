3294294689ffa3f02acfd9ed83181de6f53599b2
Yonggang Luo <luoyonggang@gmail.com>
2026-03-18 17:26:59 +0800

    dns-packet: Improve dnsResponseAnswerUpdate
    Strip boolean type from DnsServerInfoFound::resolved
    Only resolve for A and AAAA dns type
    Now dnsResponseAnswerUpdate result can be parsed easily
    Update test name for checkDomains

--- packages/dns-packet/test/dns-proxy.test.ts
+++ packages/dns-packet/test/dns-proxy.test.ts
@@ -1,7 +1,11 @@
 import assert from 'assert';
+
+import { TYPE, Packet } from '@esutils/dns-packet';
+
 import {
   updateDomainsFromLine,
   checkDomains,
+  dnsResponseAnswerUpdate,
 } from '../examples/dns-proxy-utils';
 
 const lines = `
@@ -18,7 +22,8 @@ codeload.github.com 140.82.113.10
 www.github.com 140.82.121.3
  # www.bing.com 10.10.10.10
 `.split(/\r\n|\n\r|\n|\r/);
-test('Packet#decode', () => {
+
+test('dns-proxy checkDomains test', () => {
   const x = {};
   for (let i = 0; i < lines.length; i += 1) {
     updateDomainsFromLine(x, lines[i]);
@@ -28,6 +33,7 @@ test('Packet#decode', () => {
     checkDomains(x, 'www.github.com'.split('.')),
     '140.82.121.3',
   );
+  assert.strictEqual(checkDomains(x, 'www.unknow_site.com'.split('.')), false);
   assert.strictEqual(checkDomains(x, 'github.com'.split('.')), '140.82.121.3');
   assert.strictEqual(
     checkDomains(x, 'codeload.github.com'.split('.')),
@@ -38,4 +44,106 @@ test('Packet#decode', () => {
     true,
   );
   assert.strictEqual(checkDomains(x, 'www.bing.com'.split('.')), false);
+});
+
+test('dns-proxy dnsResponseAnswerUpdate', () => {
+  assert.equal(
+    dnsResponseAnswerUpdate(
+      'github.com',
+      TYPE.A,
+      Packet.create(),
+      {
+        resolved: '8.6.6.6;7.7.7.7',
+        server: {
+          tag: '',
+          dnsList: [],
+        },
+      },
+      {
+        ip: '8.8.8.8',
+        port: 53,
+      },
+    ),
+    '8.8.8.8,github.com,A,7.7.7.7;8.6.6.6\n',
+  );
+  assert.equal(
+    dnsResponseAnswerUpdate(
+      'github.com',
+      TYPE.A,
+      Packet.create(),
+      {
+        resolved: '2001:4860:4860::8888',
+        server: {
+          tag: '',
+          dnsList: [],
+        },
+      },
+      {
+        ip: '8.8.8.8',
+        port: 53,
+      },
+    ),
+    '8.8.8.8,github.com,A,\n',
+  );
+
+  assert.equal(
+    dnsResponseAnswerUpdate(
+      'github.com',
+      TYPE.A,
+      Packet.create(),
+      {
+        resolved: '2001:4860:4860::8888',
+        server: {
+          tag: '',
+          dnsList: [],
+        },
+      },
+      {
+        ip: '8.8.8.8',
+        port: 53,
+      },
+    ),
+    '8.8.8.8,github.com,A,\n',
+  );
+
+  assert.equal(
+    dnsResponseAnswerUpdate(
+      'github.com',
+      TYPE.AAAA,
+      Packet.create(),
+      {
+        resolved: '2001:4860:4860::8888',
+        server: {
+          tag: '',
+          dnsList: [],
+        },
+      },
+      {
+        ip: '8.8.8.8',
+        port: 53,
+      },
+    ),
+    '8.8.8.8,github.com,AAAA,2001:4860:4860::8888\n',
+  );
+
+  assert.equal(
+    dnsResponseAnswerUpdate(
+      'github.com',
+      TYPE.ANY,
+      Packet.create(),
+      {
+        resolved: '2001:4860:4860::8888;8.2.3.5',
+        server: {
+          tag: '',
+          dnsList: [],
+        },
+      },
+      {
+        ip: '8.8.8.8',
+        port: 53,
+      },
+    ),
+    '8.8.8.8,github.com,ANY,8.2.3.5\n' +
+      '8.8.8.8,github.com,ANY,2001:4860:4860::8888\n',
+  );
 });
